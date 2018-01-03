171bf27ad8e15d9b8c0ad8312aedb64a907e183f
Dan Danilyuk <DanDanilyuk@gmail.com>
2018-01-03 11:39:36 -0800

    fix typo in why i built this section

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,2 +1,16 @@
 class Form < ApplicationRecord
+  validates :name_first_last, presence: true
+  validates :address_street, presence: true
+  validates :address_city, presence: true
+  validates :address_state, presence: true
+  validates :address_zip, presence: true
+  validates :phone, presence: true
+  validates :email, presence: true
+  validates :vin, presence: true
+  validates :make, presence: true
+  validates :model, presence: true
+  validates :year, presence: true
+  validates :cc, presence: true
+  validates :color, presence: true
+  validates :purchase_date, presence: true
 end
