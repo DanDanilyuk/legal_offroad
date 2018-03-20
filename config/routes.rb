f6fb9d1ddcbd9a777338e9c8c5c52c3dc47c3075
Dan Danilyuk <DanDanilyuk@gmail.com>
2018-03-20 13:13:53 -0400

    add device and rails_admin for admin login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'forms#index'
   get '/create', to: 'forms#new'
   resources :forms, only: [:index, :new, :create]
